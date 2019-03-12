4acaae5b269a205e7cd10ebdcf9f7d37a62e8df7
Turner Smith <turner.a.smith@gmail.com>
2019-03-11 23:51:21 -0500

    add simple carousel

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import Image from './components/Image';
+import Slider from 'react-slick';
 import './App.css';
 
 class App extends React.Component {
   state = {data: []};
+
+  constructor(props: any) {
+    super(props);
+  }
 
   componentDidMount() {
     this.callBackendAPI(`/api/photos/`)
@@ -32,10 +37,20 @@ class App extends React.Component {
   }
 
   render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    }
+
     return (
       <div className="app">
         <div className="app-intro">
-          {this.state.data.map((value) => this.renderImage(value))}
+          <Slider {...settings}>
+            {this.state.data.map((value) => this.renderImage(value))}
+          </Slider>
         </div>
       </div>
     );
