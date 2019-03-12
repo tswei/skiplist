b850fa67ab873676ea0d5475a9bbafb8d1baea13
Turner Smith <turner.a.smith@gmail.com>
2019-03-12 00:34:19 -0500

    add next and prev buttons as images for carousel

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,17 +4,14 @@ import Slider from 'react-slick';
 import './App.css';
 
 class App extends React.Component {
-  state = {data: []};
-
-  constructor(props: any) {
-    super(props);
-  }
+  state = {data: [], nextImage: undefined, prevImage: undefined, currentIndex: 0};
 
   componentDidMount() {
     this.callBackendAPI(`/api/photos/`)
       .then(res => {
         let dataResult = res.express.map((obj: any) => obj._id);
         this.setState({data: dataResult});
+        this.updateCurrentIndex(this, 0);
       })
       .catch(err => console.log(err));
   }
@@ -36,6 +33,72 @@ class App extends React.Component {
     }
   }
 
+  getNextImage(self: any, valueChange: number, fn: () => any) {
+    return () => {
+      self.updateCurrentIndex(self, valueChange);
+      return fn();
+    }
+  }
+
+  updateCurrentIndex(self: any, value: number) {
+    let cur = self.state.currentIndex;
+    let samples = self.state.data.length;
+    let nextIdx = (cur + value + samples) % samples;
+    let nextImage = (nextIdx + 1 + samples) % samples;
+    let prevImage = (nextIdx - 1 + samples) % samples;
+    self.setState({currentIndex: nextIdx});
+    self.callBackendAPI(`/api/photos/${self.state.data[nextImage]}`)
+      .then((res: any) => {
+        let result = res.express.image;
+        self.setState({nextImage: result});
+      })
+      .catch((err: any) => console.log(err));
+    self.callBackendAPI(`/api/photos/${self.state.data[prevImage]}`)
+      .then((res: any) => {
+        let result = res.express.image;
+        self.setState({prevImage: result});
+      })
+      .catch((err: any) => console.log(err));
+  }
+
+  SampleNextArrow(props: any) {
+    const {className, style, onClick, image, self} = props;
+    const clickFunction = self.getNextImage(self, 1, onClick);
+
+    return (
+      <div
+        className={className}
+        style={{
+          ...style,
+          display: "block",
+          opacity: ".6",
+          transform: "rotateY(-45deg)",
+        }}
+        onClick={clickFunction}>
+        <img src={image} className={"next-image"}/>
+      </div>
+    )
+  }
+
+  SamplePrevArrow(props: any) {
+    const {className, style, onClick, image, self} = props;
+    const clickFunction = self.getNextImage(self, -1, onClick);
+
+    return (
+      <div
+        className={className}
+        style={{
+          ...style,
+          display: "block",
+          opacity: ".6",
+          transform: "rotateY(45deg)",
+        }}
+        onClick={clickFunction}>
+        <img src={image} className={"prev-image"}/>
+      </div>
+    )
+  }
+
   render() {
     const settings = {
       dots: true,
@@ -43,6 +106,8 @@ class App extends React.Component {
       speed: 500,
       slidesToShow: 1,
       slidesToScroll: 1,
+      nextArrow: <this.SampleNextArrow image={this.state.nextImage} self={this}/>,
+      prevArrow: <this.SamplePrevArrow image={this.state.prevImage} self={this}/>,
     }
 
     return (
